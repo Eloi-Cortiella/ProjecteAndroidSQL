f81ff26bb3fd3ba60df72d1f0fd8a33454d2ac9b
Pablo-githubb <pablomaso@iesebre.com>
2025-11-12 19:32:49 +0100

    model

--- app/src/main/java/com/app/projecteandroidsql/utils/Constants.kt
+++ app/src/main/java/com/app/projecteandroidsql/utils/Constants.kt
@@ -1,4 +1,36 @@
 package com.app.projecteandroidsql.utils
 
 object Constants {
+    const val DATABASE_NAME = "BiblioDAMDB"
+    const val DATABASE_VERSION = 1
+
+    //Columnes taula llibres
+    const val TABLE_NAME_LLIBRES = "Llibres"
+    const val COLUM_TITOL = ""
+    const val COLUM_AUTOR = ""
+    const val COLUM_DATA_PUBLICACIO = ""
+    const val COLUM_EDITORIAL = ""
+    const val COLUM_NUM_PAGINES = ""
+    const val COLUMN_ESTAT_LLIBRE = ""
+    const val COLUMN_SINOPSIS = ""
+    const val COLUMN_PORTADA = ""
+
+
+    //Columnes taula usuaris
+    const val TABLE_NAME_USUARIS = "Usuaris"
+    const val COLUMN_USERNAME = ""
+    const val COLUMN_CONTRASENYA = ""
+    const val COLUMN_CORREU = ""
+
+
+    //Columnes taula biblioteca
+    const val TABLE_NAME_BIBLIOTECA = "Biblioteca"
+
+    //Columnes taula autor
+    const val TABLE_NAME_AUTOR = "Autor"
+    const val COLUMN_NOM = ""
+    const val COLUMN_COGNOM = ""
+    const val COLUMN_DATA_NAIXEMENT = ""
+
+
 }
