3ab1d5221d8359344b43a332ac32e0e3945e21ae
Eloi-Cortiella <eloicortiella@iesebre.com>
2025-12-27 20:34:31 +0100

    Canvis a Constants i preparant a implementar Room

--- app/src/main/java/com/app/projecteandroidsql/utils/Constants.kt
+++ app/src/main/java/com/app/projecteandroidsql/utils/Constants.kt
@@ -9,27 +9,27 @@ import com.app.projecteandroidsql.model.Llibres
  */
 object Constants {
     const val DATABASE_NAME = "BiblioDAMDB"
-    const val DATABASE_VERSION = 1
+    const val DATABASE_VERSION = 2
 
     //Columnes taula llibres
     const val TABLE_NAME_LLIBRES = "Llibres"
-    const val COLUMN_LLIBRE_ID = ""
-    const val COLUMN_TITOL = ""
-    const val COLUMN_AUTOR = ""
-    const val COLUMN_DATA_PUBLICACIO = ""
-    const val COLUMN_EDITORIAL = ""
-    const val COLUMN_NUM_PAGINES = ""
-    const val COLUMN_ESTAT_LLIBRE = ""
-    const val COLUMN_SINOPSIS = ""
-    const val COLUMN_PORTADA = ""
+    const val COLUMN_LLIBRE_ID = "id"
+    const val COLUMN_TITOL = "titol"
+    const val COLUMN_AUTOR = "autor"
+    const val COLUMN_DATA_PUBLICACIO = "data_publicacio"
+    //const val COLUMN_EDITORIAL = ""
+    const val COLUMN_NUM_PAGINES = "num_pagines"
+    const val COLUMN_ESTAT_LLIBRE = "estat"
+    const val COLUMN_SINOPSIS = "sinopsis"
+    const val COLUMN_PORTADA = "portada"
 
 
     //Columnes taula usuaris
     const val TABLE_NAME_USUARIS = "Usuaris"
-    const val COLUMN_USUARI_ID = ""
-    const val COLUMN_USERNAME = ""
-    const val COLUMN_PASSWORD = ""
-    const val COLUMN_EMAIL = ""
+    const val COLUMN_USUARI_ID = "id"
+    const val COLUMN_USERNAME = "username"
+    const val COLUMN_PASSWORD = "password"
+    const val COLUMN_EMAIL = "email"
 
 
     //Columnes taula biblioteca
@@ -39,11 +39,11 @@ object Constants {
 
     //Columnes taula autor
     const val TABLE_NAME_AUTOR = "Autor"
-    const val COLUMN_AUTOR_ID = ""
-    const val COLUMN_NOM = ""
-    const val COLUMN_COGNOM = ""
-    const val COLUMN_NOM_ARTISTIC = ""
-    const val COLUMN_DATA_NAIXEMENT = ""
+    const val COLUMN_AUTOR_ID = "id"
+    const val COLUMN_NOM = "nom"
+    const val COLUMN_COGNOM = "cognom"
+    const val COLUMN_NOM_ARTISTIC = "nom_artistic"
+    const val COLUMN_DATA_NAIXEMENT = "data_naixement"
 
 
 }
